410cf369b812ce8b98f2359d095b464722760ae4
Ariel Espindola <espindola_ipn@hotmail.com>
2021-02-25 15:35:51 -0600

    Second try - approach B

--- main.cpp
+++ main.cpp
@@ -18,12 +18,15 @@ using namespace std;
 typedef struct Street
 {
     int B, E, L;
+    // frequency of the street in a path
+    int freqInPath;
     string streetName;
     
     void read(){
         cin >> B >> E;
         cin >> streetName;
         cin >> L;
+        freqInPath = 0;
     }
 }street_t;
 
@@ -51,19 +54,79 @@ public:
 
 
     // all intersections with one
-    void approachA(unordered_map<int, unordered_map<string, int>>& intersections)
+    void approachA(vector<street_t>& streets, 
+        unordered_map<int, unordered_map<string, int>>& intersections)
     {
+        
         printIntersections(intersections);
     }
 
-
-    void solve( vector<car_t>& cars, vector<street_t>& streets,  
-        unordered_map<int, unordered_map<string, int>>& intersections)
+    // Adding new integer memeber to streets structure
+    // integer: number of times that the street appears in a path
+    // 
+    void approachB( vector<car_t>& cars, vector<street_t>& streets,
+        unordered_map<int, unordered_map<string, int>>& intersections )
     {
-        // TODO
+        unordered_map<string,int> freqMap;
+        // 0: street is never in the path of a car
+        // 1: street is at least once in the path of a car
+        // 2: street is the MOST frequent street in the path of a car
+        for ( const car_t& car : cars )
+        {
+            fillStreetFreqInPath(car.path, freqMap);
+        }
+
+        #ifdef DEBUG
+        // printing the frequency map
+        for ( auto& [streetName, freq] : freqMap )
+        {
+            cout << streetName << " : " << freq << endl;
+        }
+        #endif
+
+        // Fill the intersections with 0
+        for ( const street_t& street: streets )
+        {
+            intersections[street.E][street.streetName] = 0;
+        }
+
+        // write some intersections
+        for ( auto& [id, umapStreetTime] : intersections )
+        {
+            pair<string, int> best {"", 0};
+            for ( auto& [streetName, timeInGreen] : umapStreetTime )
+            {
+                if ( freqMap.find(streetName) != freqMap.end() )
+                {
+                    intersections[id][streetName] = 1;
+                }
+
+                 if ( (freqMap.find(streetName) != freqMap.end()) 
+                    && freqMap[streetName] > best.second )
+                {
+                    best.first = streetName;
+                    best.second = freqMap[streetName];
+                }
+            }
+            // at this point, best contains the best pair
+            // cout << best.first << " : " << best.second << endl;
+            intersections[id][best.first] = 2;
+        }
+
+        // delete elements which count is 0
+        // if intersections[id][best.first] == 0, delete entry
+
+
+        printIntersections(intersections);
     }
 
 private:
+
+    // count the number of streets in the paths
+    void fillStreetFreqInPath(vector<string> path, unordered_map<string,int>& umap)
+    {
+        for ( const string& streetName : path ) umap[streetName]++;
+    }
 
     void printIntersections(
         unordered_map<int, unordered_map<string, int>>& intersections)
@@ -73,8 +136,22 @@ private:
         for ( auto& [id, umapStreetTime] : intersections )
         {
             cout << id << endl;
-            cout << (int) umapStreetTime.size() << endl;
+            int size = (int) umapStreetTime.size();
+            unordered_map<string,int> stNameTimeMap;
+            // cout << (int) umapStreetTime.size() << endl;
             for ( auto& [streetName, timeInGreen] : umapStreetTime )
+            {
+                if (timeInGreen == 0 ) size--;
+                else
+                {
+                    stNameTimeMap[streetName] = timeInGreen;
+                }
+                // cout << streetName << " " << timeInGreen << endl;
+            }
+
+            cout << stNameTimeMap.size() << endl;
+
+            for ( auto& [streetName, timeInGreen] : stNameTimeMap )
             {
                 cout << streetName << " " << timeInGreen << endl;
             }
@@ -84,8 +161,8 @@ private:
 
 int main ()
 {
-    freopen("inputs/f.txt", "r", stdin);
-    freopen("outputs/f.txt", "w", stdout);
+    freopen("inputs/b.txt", "r", stdin);
+    freopen("outputs/b.txt", "w", stdout);
 
     int D, I, S, V, F;
     cin >> D >> I >> S >> V >> F;
@@ -100,7 +177,6 @@ int main ()
         cout << street.B << " " << street.E << " " \
         << street.streetName << " " << street.L << endl;
         #endif
-        intersections[street.E][street.streetName] = 1;
     }
 
     vector<car_t> cars(V);
@@ -115,9 +191,10 @@ int main ()
         #endif
     }
 
+
     Solution sol;
-    // t = 5
-    sol.approachA(intersections);
+    // sol.approachA(intersections);
+    sol.approachB(cars, streets, intersections);
 
     return 0;
 }
